475b205f7de9a0b7f98387bc0fadc20d79d821b6
Nir Oren <niro@edev.co>
2024-04-11 01:03:07 +0300

    user agent bit older chrome

--- src/config/WAuserAgente.ts
+++ src/config/WAuserAgente.ts
@@ -1,3 +1,3 @@
 const useragentOverride =
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36';
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36';
 export { useragentOverride };
